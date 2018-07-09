9d2c3e09ca79481c69077dde176a37b82a21a2f4
sam46 <abdlwahdsa@gmail.com>
2018-07-09 01:01:48 -0400

    add ADT + biolerplate parsers for block, program, statment, Const

--- src/Paskell.hs
+++ src/Paskell.hs
@@ -8,13 +8,24 @@ import Utils (parse')
 
 data Reserved = KWvar | KWtype | KWconst | KWbegin | KWend |
     KWprogram deriving (Show)
+
 data Ident = Ident String deriving (Show)
 data Type = TYident Ident | TYchar | TYboolean |
     TYinteger | TYreal | TYstring deriving (Show)
 data IdentList = IdentList [Ident]     deriving (Show)
-data Decl = DeclVar [VarDecl] | DeclType [TypeDecl]  deriving (Show)
+
+data Decl = DeclVar [VarDecl] | DeclType [TypeDecl] |
+    DeclConst [ConstDecl] deriving (Show)
 data VarDecl = VarDecl IdentList Type     deriving (Show)
 data TypeDecl = TypeDecl Ident Type     deriving (Show)
+data ConstDecl = ConstDecl deriving (Show) -- todo 
+
+data Program = Program Ident Block deriving (Show)
+data Block = Block [Decl] [Statement] deriving (Show)
+
+data Statement = Statement deriving (Show) -- todo
+
+------------------------------------------------------
 
 parseReserved :: Parser Reserved
 parseReserved = undefined -- todo
@@ -59,7 +70,6 @@ parseVarDecl = do
         between spaces spaces (char ';')
         return $ VarDecl idents t
 
-
 parseTypeDecl :: Parser [TypeDecl]
 parseTypeDecl = do
     between spaces (many1 space) (stringIgnoreCase "type")
@@ -69,3 +79,32 @@ parseTypeDecl = do
         t <- parserType
         between spaces spaces (char ';')
         return $ TypeDecl ident t
+
+parseConstDecl :: Parser [ConstDecl]
+parseConstDecl = undefined -- todo
+
+
+parseProgram :: Parser Program
+parseProgram = do
+    between spaces (many1 space) (stringIgnoreCase "program")
+    ident <- parseIdent
+    between spaces spaces (char ';')
+    block <- parseBlock
+    between spaces spaces (char '.')
+    return $ Program ident block
+
+parseBlock :: Parser Block
+parseBlock = do
+    decls <- between spaces spaces parseDecl
+    return $ Block decls [] -- todo [Statements]
+
+
+parseDecl :: Parser [Decl]
+parseDecl = many $
+    (parseTypeDecl >>= \xs -> return $ DeclType xs) <|>
+    (parseVarDecl >>= \xs -> return $ DeclVar xs) <|>
+    (parseConstDecl >>= \xs -> return $ DeclConst xs)
+
+
+parserStatement :: Parser Statement
+parserStatement = undefined -- todo
