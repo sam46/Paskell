191c0db97ac9f83539206dec14190b91d7014614
sam46 <abdlwahdsa@gmail.com>
2018-07-10 13:48:34 -0400

    fixed identifier, identlist and VarDecl parsers

--- src/Paskell.hs
+++ src/Paskell.hs
@@ -4,81 +4,38 @@ import Text.Parsec
 import Text.Parsec.String
 import Text.Parsec.Combinator
 import Data.Char
-import Utils (parse')
-
-data Reserved = KWvar | KWtype | KWconst | KWbegin | KWend |
-    KWprogram deriving (Show)
-
-data Ident = Ident String deriving (Show)
-data Type = TYident Ident | TYchar | TYboolean |
-    TYinteger | TYreal | TYstring deriving (Show)
-data IdentList = IdentList [Ident]     deriving (Show)
-
-data Decl = DeclVar [VarDecl] | DeclType [TypeDecl] |
-    DeclConst [ConstDecl] deriving (Show)
-data VarDecl = VarDecl IdentList Type     deriving (Show)
-data TypeDecl = TypeDecl Ident Type     deriving (Show)
-data ConstDecl = ConstDecl deriving (Show) -- todo 
-
-data Program = Program Ident Block deriving (Show)
-data Block = Block [Decl] [Statement] deriving (Show)
-
-data Statement = Statement deriving (Show) -- todo
+-- import our own modules
+import Grammar
+import ExtraParsers
+import KeywordParse
+import Utils (p')
 
 ------------------------------------------------------
-
-comments :: Parser ()
-comments = spaces -- todo change to actual comment parser
-
-whitespace :: Parser ()
-whitespace = do {spaces; comments; spaces; comments}
-
-tok :: Parser a -> Parser a
-tok p = p >>= \x -> spaces >>= \_ -> return x 
-
-charTok :: Char -> Parser Char
-charTok = tok . char  
-
-commaTok :: Parser Char
-commaTok = charTok ','
-
-semicolTok :: Parser Char
-semicolTok = charTok ';'
-
-charIgnoreCaseTok :: Char -> Parser Char
-charIgnoreCaseTok = charTok (toUpper c) <|> charTok (toLower c)
-
-stringIgnoreCaseTok :: String -> Parser String
-stringIgnoreCaseTok = 
-
-charIgnoreCase :: Char -> Parser Char
-charIgnoreCase c = char (toUpper c) <|> char (toLower c)
-
-stringIgnoreCase :: String -> Parser String
-stringIgnoreCase [] = return []
-stringIgnoreCase (x:xs) = do {c <- charIgnoreCase x; cs <- stringIgnoreCase xs; return $ [c] ++ cs}
+-- make sure keywords are lower-cased
+keywords = ["and","downto","if","or",
+    "then","array","else","in","packed",
+    "to","begin","end","label","procedure",
+    "type","case","file","mod","program","until",
+    "const","for","nil","record","var",
+    "div","function","not","repeat","while",
+    "do","goto","of","set","with"]
+special = [":=","+","-","*","/","="
+    ,"<",">","<>","<=",">=","(",")","[",
+    "]",",",".",";",":","..","^"]
 
 
-
-
-------------------------------------------------------
-reserved = ["var", "char"]
-
-parseReserved :: Parser Reserved
-parseReserved = undefined -- todo
 
 parseIdent :: Parser Ident
-parseIdent = fff -- <?> "reserved kw " --  <|> (unexpected $ "error: " ++ " is a reserved keyword")
-    where fff = do
-                l <- letter
-                rest <- many alphaNum
-                if not (elem ([l] ++ rest) reserved) then return $ Ident $ [l] ++ rest else parserZero
-    -- todo exclude Reserved keywords
-
-
+parseIdent = tok . try $ do
+    x <- letter
+    xs <- many alphaNum
+    let ident = x:xs
+    if (toLower <$> ident) `elem` keywords 
+        then fail ("Expecting identifier but found keyword " ++ ident)
+        else return (Ident ident) 
 
 parserType :: Parser Type
-parserType = 
+parserType = tok $ 
     (stringIgnoreCase "boolean" >> return TYboolean) <|>
     (stringIgnoreCase "integer" >> return TYinteger) <|>
     (stringIgnoreCase "real" >> return TYreal) <|>
@@ -87,62 +44,55 @@ parserType =
     (parseIdent >>= \x -> return $ TYident x)
 
 parseIdentList :: Parser IdentList
-parseIdentList = IdentList <$> sepBy1 parseIdent sep
-    where sep = try (spaces >> char ',' >> spaces)
+parseIdentList = IdentList <$> sepBy1 parseIdent commaTok
 
 parseVarDecl :: Parser [VarDecl]
-parseVarDecl = do
-    try $ between spaces (many1 space) (stringIgnoreCase "var")
-    many1 $ do
-        -- spaces
-        idents <- parseIdent -- parseIdentList
-        between spaces spaces (char ':')
-        t <- parserType
-        -- between spaces spaces (char ';')
-        spaces
-        char ';'
-        return $ VarDecl (IdentList [idents]) t
-
-parseTypeDecl :: Parser [TypeDecl]
-parseTypeDecl = do
-    between spaces (many1 space) (stringIgnoreCase "type")
-    many1 $ do
-        spaces
-        ident <- parseIdent
-        between spaces spaces (char '=')
-        t <- parserType
-        -- between spaces spaces (char ';')
-        spaces 
-        (char ';')
-        return $ TypeDecl ident t
-
-parseConstDecl :: Parser [ConstDecl]
-parseConstDecl = undefined -- todo
+parseVarDecl = (parseKWvar <?> "expecting keyword 'var'") 
+    >> ((many1 $ try  -- todo try separating many1 into initial parse and then many for better error messages
+        (do {l <- parseIdentList; charTok ':'; t <- parserType; semicolTok; return $ VarDecl l t})
+        ) <?> "Missing or incorrect variable declaration")
 
 
-parseProgram :: Parser Program
-parseProgram = do
-    between spaces (many1 space) (stringIgnoreCase "program")
-    ident <- parseIdent
-    between spaces spaces (char ';')
-    block <- parseBlock
-    between spaces spaces (char '.')
-    -- try (spaces >> char '.')
-    -- try (char '.')
-    return $ Program ident block
+-- parseTypeDecl :: Parser [TypeDecl]
+-- parseTypeDecl = do
+--     between spaces (many1 space) (stringIgnoreCase "type")
+--     many1 $ do
+--         spaces
+--         ident <- parseIdent
+--         between spaces spaces (char '=')
+--         t <- parserType
+--         -- between spaces spaces (char ';')
+--         spaces 
+--         (char ';')
+--         return $ TypeDecl ident t
 
-parseBlock :: Parser Block
-parseBlock = do
-    decls <- between spaces spaces parseDecl
-    return $ Block decls [] -- todo [Statements]
-
-
-parseDecl :: Parser [Decl]
-parseDecl = many $
-    -- (parseTypeDecl >>= \xs -> return $ DeclType xs) <|>
-    (parseVarDecl >>= \xs -> return $ DeclVar xs) -- <|>
-    -- (parseConstDecl >>= \xs -> return $ DeclConst xs)
+-- parseConstDecl :: Parser [ConstDecl]
+-- parseConstDecl = undefined -- todo
 
 
-parserStatement :: Parser Statement
-parserStatement = undefined -- todo
+-- parseProgram :: Parser Program
+-- parseProgram = do
+--     between spaces (many1 space) (stringIgnoreCase "program")
+--     ident <- parseIdent
+--     between spaces spaces (char ';')
+--     block <- parseBlock
+--     between spaces spaces (char '.')
+--     -- try (spaces >> char '.')
+--     -- try (char '.')
+--     return $ Program ident block
+
+-- parseBlock :: Parser Block
+-- parseBlock = do
+--     decls <- between spaces spaces parseDecl
+--     return $ Block decls [] -- todo [Statements]
+
+
+-- parseDecl :: Parser [Decl]
+-- parseDecl = many $
+--     -- (parseTypeDecl >>= \xs -> return $ DeclType xs) <|>
+--     (parseVarDecl >>= \xs -> return $ DeclVar xs) -- <|>
+--     -- (parseConstDecl >>= \xs -> return $ DeclConst xs)
+
+
+-- parserStatement :: Parser Statement
+-- parserStatement = undefined -- todo
