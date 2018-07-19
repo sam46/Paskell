3640f289ddf11b546b2818b697c6469b0145190c
sam46 <abdlwahdsa@gmail.com>
2018-07-19 00:54:09 -0400

    Rewrite Type Checker using Monads

--- src/TypeCheck.hs
+++ src/TypeCheck.hs
@@ -7,106 +7,89 @@ import Data.List
 
 type Env = [(Ident, Type)]
 
-isLeft :: (Either a b) -> Bool 
-isLeft (Left _) = True
-isLeft _        = False
-pickLeft x y = if isLeft x then x else y
-fromRight (Right x) = x
+-- isLeft :: (Either a b) -> Bool 
+-- isLeft (Left _) = True
+-- isLeft _        = False
+-- pickLeft x y = if isLeft x then x else y
+-- fromRight (Right x) = x
 
-tynum = [TYint, TYreal] 
+isNum = (`elem` [TYint, TYreal])
+
+-- Convert a variable lookup from Maybe to Either
+lookupIdent :: Ident -> Env -> Either String Type
+lookupIdent x env = case (lookup x env)  of 
+    Nothing -> Left $ "Unknown variable " ++ show x
+    Just t  -> Right t
 
 typechk :: Env -> Statement -> Either String Env
 typechk env (Assignment (Designator x _) expr) = 
-    case gettype env expr of
-        Left err -> Left err
-        Right v ->
-            case (lookup x env)  of 
-                Just xtype -> if xtype == v then Right env else
-                              Left $ "Can't assign " ++ (show v) ++ "to " ++ (show xtype) 
-                _          -> Left $ "Unknown variable " ++ (show x)
+    gettype env expr >>=  \t -> 
+        lookupIdent x env >>=
+            \xtype -> if xtype == t
+                      then Right env 
+                      else Left $ "Can't assign " ++ 
+                           (show t) ++ " to " ++ show xtype
 
 typechk env (StatementIf expr s1 ms2) =
-    case gettype env expr of
-        Left err -> Left err
-        Right v ->
-            if v /= TYbool 
-            then Left "If condition must be boolean" 
-            else let tchk1 = typechk env s1
-                     tchk2 = case ms2 of Just s2 -> Just $ typechk env s2
-                                         Nothing -> Nothing
-            in case (tchk1, tchk2) of
-                (Left err, _) -> Left err
-                (_, Just (Left err)) -> Left err
-                _ -> Right env
+    gettype env expr >>= \t ->
+        if t /= TYbool 
+        then Left $ "Expecting boolean in IF condition, got " ++ show t 
+        else let tchk1 = typechk env s1
+                 mtchk2 = (typechk env) <$> ms2
+            in case mtchk2 of
+            Just tchk2 -> tchk1 >> tchk2 >> Right env  
+            Nothing    -> tchk1 >> Right env
 
 gettype :: Env -> Expr -> Either String Type
-gettype env (FactorInt _ ) = Right TYint
-gettype env (FactorReal _) = Right TYreal
-gettype env (FactorStr _) = Right TYstr
-gettype env FactorTrue = Right TYbool
-gettype env FactorFalse = Right TYbool
-gettype env (FactorNot x) = undefined
+gettype env FactorTrue          = Right TYbool
+gettype env FactorFalse         = Right TYbool
+gettype env (FactorInt _)       = Right TYint
+gettype env (FactorReal _)      = Right TYreal
+gettype env (FactorStr _)       = Right TYstr
+gettype env (FactorNot x)       = undefined
 gettype env (FactorFuncCall fc) = undefined
 
 gettype env (FactorDesig (Designator x _)) = 
-    case lookup x env of 
-        Nothing -> Left $ "Unknown identifier " ++ (show x) 
-        Just v  -> Right v
+    lookupIdent x env
 
 gettype env (Unary op x) = 
-    case t of Left _ -> t
-              Right TYint -> t
-              Right TYreal -> t
-              _            -> Left $ (show op) ++ " operand must be integer or real" 
-    where t = gettype env x
+    gettype env x >>= \t ->
+        if isNum t then Right t 
+        else Left $ (show op) ++ " operand must be integer or real"
+
+gettype env (Relation x1 op x2) =
+    t1 >>= \v1 -> t2 >>= \v2 ->
+        if (v1 == v2) || (isNum v1 && isNum v2)
+        then Right TYbool
+        else Left $ (show op) ++ " operands must be boolean"
+    where [t1, t2] = (gettype env) <$> [x1, x2]
 
 gettype env (Add x1 op x2)
     | op `elem` [OPplus, OPminus] =
-        case (t1, t2) of 
-            (Right v1, Right v2) -> 
-                if (v1 `notElem` tynum) || (v2 `notElem` tynum)
-                then Left $ (show op) ++ " operands must be integer or real"
-                else if v1 == TYreal then t1 else t2
-            _                    -> pickLeft t1 t2
+        t1 >>= \v1 -> t2 >>= \v2 ->
+            if not (isNum v1 && isNum v2)
+            then Left $ (show op) ++ " operands must be integer or real"
+            else if v1 == TYreal then t1 else t2
     | otherwise = 
-        case (t1, t2) of
-            (Right v1, Right v2) -> 
-                if (v1 /= TYbool) || (v2 /= TYbool)
-                then Left $ (show op) ++ " operands must be boolean"
-                else t1
-            _                    -> pickLeft t1 t2
-    where t1 = gettype env x1
-          t2 = gettype env x2
+        t1 >>= \v1 -> t2 >>= \v2 ->
+            if (v1 /= TYbool) || (v2 /= TYbool)
+            then Left $ (show op) ++ " operands must be boolean"
+            else t1
+    where [t1, t2] = (gettype env) <$> [x1, x2]
 
 gettype env (Mult x1 op x2)
     | op `elem` [OPstar, OPdiv] =
-        case (t1, t2) of 
-            (Right v1, Right v2) -> 
-                if (v1 `notElem` tynum) || (v2 `notElem` tynum)
-                then Left $ (show op) ++ " operands must be integer or real"
-                else if v1 == TYreal then t1 else t2
-            _                    -> pickLeft t1 t2
+        t1 >>= \v1 -> t2 >>= \v2 ->
+            if not (isNum v1 && isNum v2)
+            then Left $ (show op) ++ " operands must be integer or real"
+            else if v1 == TYreal then t1 else t2
     | otherwise = 
-        case (t1, t2) of 
-            (Right v1, Right v2) -> 
-                if (v1 /= TYbool) || (v2 /= TYbool)
-                then Left $ (show op) ++ " operands must be boolean"
-                else t1
-            _                    -> pickLeft t1 t2
-    where t1 = gettype env x1
-          t2 = gettype env x2
-
-gettype env (Relation x1 op x2) = -- todo more intricate type comparsions
-    case (t1, t2) of 
-        (Right v1, Right v2) -> 
-            if (v1 == v2) ||
-               ((v1 `elem` tynum) && (v2 `elem` tynum)) ||
-               ((v1 `elem` tynum) && (v2 `elem` tynum))
-            then Right TYbool
-            else Left $ (show op) ++ " operands must be boolean"
-        _  -> pickLeft t1 t2
-    where t1 = gettype env x1
-          t2 = gettype env x2
+        t1 >>= \v1 -> t2 >>= \v2 ->
+            if (v1 /= TYbool) || (v2 /= TYbool)
+            then Left $ (show op) ++ " operands must be boolean"
+            else t1
+    where [t1, t2] = (gettype env) <$> [x1, x2]
 
 typechkStr s env = case p' parseStatement s of 
-        Right st -> typechk env st
+    Right st -> typechk env st
+    Left err -> error $ "Parse error:\n\t" ++ s ++ "\nin:" ++ show err
